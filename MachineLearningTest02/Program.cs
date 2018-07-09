711c90d0720d20b199864563583f4c80a448b0b9
Maamar Dliouah <maamardli@windowslive.com>
2018-07-09 20:48:01 +0100

    Add packages and data files

--- MachineLearningTest02/Program.cs
+++ MachineLearningTest02/Program.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
 
 namespace MachineLearningTest02
 {
