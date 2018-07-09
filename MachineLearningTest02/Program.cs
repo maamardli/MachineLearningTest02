16a7fd14b0e0b54de3564e92909a531555a9e7f5
Maamar Dliouah <maamardli@windowslive.com>
2018-07-09 23:52:49 +0100

    finishing the sample

--- MachineLearningTest02/Program.cs
+++ MachineLearningTest02/Program.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Models;
-using Microsoft.ML.Runtime;
-using Microsoft.ML.Runtime.Api;
 using Microsoft.ML.Trainers;
 using Microsoft.ML.Transforms;
 
@@ -17,34 +11,46 @@ namespace MachineLearningTest02
 {
 	class Program
 	{
-		static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
-		static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
-		static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+		private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+		private static string _dataPath => Path.Combine(AppPath, "datasets", "taxi-fare-train.csv");
+		private static string _testDataPath => Path.Combine(AppPath, "datasets", "taxi-fare-test.csv");
+		private static string _modelPath => Path.Combine(AppPath, "TaxiFareModel.zip");
+
 
 		static async Task Main(string[] args)
 		{
+			Console.WriteLine("loading the model...");
 			PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = await Train();
+			Console.WriteLine("Evaliationg...");
 			Evaluate(model);
+			TaxiTripFarePrediction prediction = model.Predict(TestTaxiTrips.Trip1);
+			Console.WriteLine("Predicted fare: {0}, actual fare: 29.5", prediction.FareAmount);
+			Console.ReadLine();
 		}
 
 		public static async Task<PredictionModel<TaxiTrip, TaxiTripFarePrediction>> Train()
 		{
+			Console.WriteLine("1");
 			var pipeline = new LearningPipeline
 			{
-				new TextLoader(_dataPath).CreateFrom<TaxiTrip>(useHeader: true, separator: ','),
-				new ColumnCopier("FareAmount", "Label"),
-				new CategoricalOneHotVectorizer("VendorId", "RateCocde", "PaymentType"),
-				new ColumnConcatenator("Feature", "VendorId", "RateCode", "PassengerCount", "TripDistance", "PaymentType"),
+
+				new TextLoader(_dataPath).CreateFrom<TaxiTrip>(separator: ','),
+				new ColumnCopier(("FareAmount", "Label")),
+				new CategoricalOneHotVectorizer("VendorId", "RateCode", "PaymentType"),
+				new ColumnConcatenator("Features", "VendorId", "RateCode", "PassengerCount", "TripDistance", "PaymentType"),
 				new FastTreeRegressor(),
 			};
-			PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = pipeline.Train<TaxiTrip, TaxiTripFarePrediction>();
+			Console.WriteLine("2");
+			var model = pipeline.Train<TaxiTrip, TaxiTripFarePrediction>();
+			Console.WriteLine("3");
 			await model.WriteAsync(_modelPath);
+			Console.WriteLine("4");
 			return model;
 		}
 
 		public static void Evaluate(PredictionModel<TaxiTrip, TaxiTripFarePrediction> model)
 		{
-			var testData = new TextLoader(_testDataPath).CreateFrom<TaxiTrip>(useHeader: true, separator: ',');
+			var testData = new TextLoader(_testDataPath).CreateFrom<TaxiTrip>(separator: ',');
 			var evaluator = new RegressionEvaluator();
 			RegressionMetrics metrics = evaluator.Evaluate(model, testData);
 			Console.WriteLine($"Rms = {metrics.Rms}");
