72f0888e6ea7deadfa2a0574042a57a191a25886
Maamar Dliouah <maamardli@windowslive.com>
2018-07-09 22:53:45 +0100

    Create classes and the main block of the  code

--- MachineLearningTest02/Program.cs
+++ MachineLearningTest02/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ML;
 using Microsoft.ML.Data;
@@ -15,8 +17,38 @@ namespace MachineLearningTest02
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+		static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+		static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+
+		static async Task Main(string[] args)
 		{
+			PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = await Train();
+			Evaluate(model);
+		}
+
+		public static async Task<PredictionModel<TaxiTrip, TaxiTripFarePrediction>> Train()
+		{
+			var pipeline = new LearningPipeline
+			{
+				new TextLoader(_dataPath).CreateFrom<TaxiTrip>(useHeader: true, separator: ','),
+				new ColumnCopier("FareAmount", "Label"),
+				new CategoricalOneHotVectorizer("VendorId", "RateCocde", "PaymentType"),
+				new ColumnConcatenator("Feature", "VendorId", "RateCode", "PassengerCount", "TripDistance", "PaymentType"),
+				new FastTreeRegressor(),
+			};
+			PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = pipeline.Train<TaxiTrip, TaxiTripFarePrediction>();
+			await model.WriteAsync(_modelPath);
+			return model;
+		}
+
+		public static void Evaluate(PredictionModel<TaxiTrip, TaxiTripFarePrediction> model)
+		{
+			var testData = new TextLoader(_testDataPath).CreateFrom<TaxiTrip>(useHeader: true, separator: ',');
+			var evaluator = new RegressionEvaluator();
+			RegressionMetrics metrics = evaluator.Evaluate(model, testData);
+			Console.WriteLine($"Rms = {metrics.Rms}");
+			Console.WriteLine($"Rsquared = {metrics.RSquared}");
 		}
 	}
 }
---
+++ MachineLearningTest02/TaxiTripFarePrediction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Runtime.Api;
+
+namespace MachineLearningTest02
+{
+	class TaxiTripFarePrediction
+	{
+		[ColumnName("Score")]
+		public float FareAmount;
+	}
+}
